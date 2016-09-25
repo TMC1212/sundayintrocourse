5d437303f08776a9b6b3040c23f4495004fec5ff
Tina Cummins <Tina Cummins>
2016-09-25 14:28:48 -0600

    Added playing loop and exits

--- wordgame.py
+++ wordgame.py
@@ -1,5 +1,25 @@
-def playwordgames():
-    print("Hey world")
+def get_random_word():
+    return "pizza"
 
-print("Playing")
-playwordgames()
+def play_word_games():
+    strikes = 0
+    max_strikes = 3
+    playing = True
+
+    word = get_random_word()
+
+    while playing:
+        strikes += 1
+
+        if strikes >= max_strikes:
+            playing = False
+
+    if strikes >= max_strikes:
+        print("Sorry, game over")
+    else:
+        print("Winner!")
+     
+
+print("Start the Game")
+play_word_games()
+print("End the Game")
