f0649d7e0f71bbeeed6dd97d96fe1b19e23a4eb2
Tina Cummins <Tina Cummins>
2016-09-25 16:15:24 -0600

    Finished game!!!

--- wordgame.py
+++ wordgame.py
@@ -10,6 +10,26 @@ def show_word(word):
         print(character," ",end="")
     print("")
 
+def get_guess():
+    print("Enter a letter")
+    return input()
+
+def process_letter(letter, secret_word, blanked_word):
+    result = False
+
+    for i in range(0, len(secret_word)):
+        if secret_word[i] == letter:
+            result = True
+            blanked_word[i] = letter
+
+    return result
+
+def print_strikes(strikes):
+    for i in range(0, strikes):
+        print("X", end="")
+    print("")
+
+
 def play_word_games():
     strikes = 0
     max_strikes = 3
@@ -17,16 +37,22 @@ def play_word_games():
 
     word = get_random_word()
 
-    blanked_word = "_" * len(word)
+    blanked_word = list("_" * len(word))
 
     while playing:
         show_word(blanked_word)
 
-        #letter = get_guess()
+        letter = get_guess()
+        found = process_letter(letter, word, blanked_word)
 
-        strikes += 1
+        if not found:
+            strikes += 1
+            print_strikes(strikes)
 
         if strikes >= max_strikes:
+            playing = False
+
+        if not "_" in blanked_word:
             playing = False
 
     if strikes >= max_strikes:
